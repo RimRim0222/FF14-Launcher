0be053e8167f2057014d79cae23cc1c1d3d4e084
RimRim0222 <45350582+RimRim0222@users.noreply.github.com>
2020-06-18 02:36:44 +0900

    FF14-Launcher 1

---
+++ js/launcher.js
@@ -0,0 +1,259 @@
+// modal position
+function modalCenter() {
+    var modal = $(".modal");
+    var w_width = $(window).width();
+    var w_height = $(window).height();
+    //console.log(w_width, w_height)
+    for(var i=0; i<modal.length; i++) {
+        var m_width = modal.eq(i).width();
+        var m_height = modal.eq(i).height();
+        //console.log(m_width, m_height)
+        modal.eq(i).css({
+            left: (w_width - m_width)/2,
+            top: (w_height - m_height)/2
+        });
+    }
+}
+
+// 무료체험 텍스트
+function trialText() {
+    $(".trial").fadeIn();
+}
+
+// resizing할 요소들
+function resizing() {
+    // 무료체험 텍스트 좌표
+    setTimeout(function() {
+        $(".trial").css({
+            width: $("#content-blocks").width()
+        });
+    }, 200);
+}
+
+// scroll-js
+function scrollJS() {
+    $(".scroll-js-horizon").mCustomScrollbar({theme:"default", axis:"x"});
+    $(".scroll-js").mCustomScrollbar({theme:"default", axis:"y"});
+}
+
+// toast popup
+var toast;
+var toastLength;
+//setTimeout(toastOpen, 2000);
+function toastOpen() { //toast open
+    toast = $("#toast li");
+    toastLength = toast.length;
+    //toast.parents("#toast").css({right: 15});
+    $("#toast").css({
+        display: 'block',
+        right: 15
+    });
+    toast.css({
+        right: 0,
+        top: 300,
+        opacity: 1
+    });
+    var toastAction = setInterval(function(){ 
+        toastLength -= 1; 
+        console.log(toastLength)
+        toast.eq(toastLength).animate({
+            top: 0,
+            opacity: 1
+        }, 400, "easeOutBack");      
+        if (toastLength <= 0) {
+            clearInterval(toastAction);
+            setTimeout(toastRemove, 15000);
+        }
+    }, 300)
+};
+function toastRemove() { //toast remove
+    toast = $("#toast li");
+    toastLength = toast.length;
+    var toastAction = setInterval(function(){ 
+        toastLength -= 1;    
+        console.log(toastLength)
+        toast.eq(toastLength).animate({
+            right: -400,
+            opacity: 0
+        }, 600, "easeOutQuart");   
+        if (toastLength <= 0) {
+            clearInterval(toastAction);
+            $("#toast").fadeOut();
+        }
+    }, 100);        
+};
+function callToast(){
+    setTimeout(toastOpen, 1000);
+}
+
+// 팝업 내 동영상 멈추기
+function stopVideo(_modal) {
+    //var div = e.target.closest(".modal");
+    console.log(_modal)
+    var div = _modal;
+    var iframee = div.getElementsByTagName("iframe")[0].contentWindow;
+    iframee.postMessage('{"event":"command","func":"pauseVideo","args":""}','*');
+}
+
+//창 최대화/최소화 toggle
+function toggle_maximum(_this){
+    var $this = $(_this);
+    if ($this.hasClass("type2")) {
+        $this.removeClass("type2")
+    } else {
+        $this.addClass("type2")
+    }
+}
+
+// window load 후
+function afterLoad() {
+    $("#content-blocks").masonry({
+        itemSelector : ".block", 
+        columnWidth : 488,
+        horizontalOrder: true,
+        isFitWidth: true,
+        stagger: 45,
+        transitionDuration: '0.3s',
+        setLeft: 0
+    });
+}
+
+$("document").ready(function(){
+    //scrollbar, masonry
+    //$(".scroll-js-horizon").mCustomScrollbar({theme:"default", axis:"x"});
+    //$(".scroll-js").mCustomScrollbar({theme:"default", axis:"y"});
+    scrollJS();
+    $("#content-blocks").masonry({
+        itemSelector : ".block", 
+        columnWidth : 488,
+        horizontalOrder: true,
+        isFitWidth: true,
+        stagger: 30,
+        transitionDuration: '0.3s'
+    });
+    modalCenter();
+
+    // LNB 펼치기/접기
+    var lnb = $("#lnb");
+    var btnFold = $(".btn_fold");
+    var sns = $(".sns_wrap");
+    btnFold.on("click", function(){
+        if (lnb.hasClass("fold")) { 
+            //sns.animate({width: 265}, 200, "easeOutQuint");       
+            lnb.animate({width: 265}, 200, "easeOutQuint", function(){
+                lnb.removeClass("fold");
+            });
+            btnFold.removeClass("on");
+        } else {
+            //sns.animate({width: 130}, 200, "easeOutQuint");
+            lnb.animate({width: 130}, 200, "easeOutQuint", function(){
+            }).addClass("fold");
+            btnFold.addClass("on");
+        }
+    });
+
+    // LNB내 PC방 혜택 아코디언
+    var foldPc = $("#foldPc");
+    var follWrap = foldPc.closest("#lnb .list.pc");
+    var pcAccodian = $("#pcAccodian");
+    pcAccodian.on("click", function(){
+        if (follWrap.hasClass("fold")) {
+            follWrap.removeClass("fold");
+            foldPc.siblings().animate({height: foldPc.height(), martinTop: 4}, 200, "easeOutCubic");
+        } else {
+            follWrap.addClass("fold");
+            foldPc.siblings().animate({height: 0, martinTop: 0}, 200, "easeOutCubic");
+        }
+    });
+
+    // toast close
+    $("#toast .close").on("click", function(e){
+        //console.log(e.target)
+        $(e.target).closest("li").hide();
+    });
+
+    // modal 제어
+    $(".modalController").on("click", function(e){
+        var $this = $(e.target).closest(".modalController");
+        console.log($this, openTarget)
+        var openTarget = $this.attr("modal-target");
+        var closeTarget = $this.closest(".modal");
+        if ($this.attr("modal-control") == "open") {
+            modalCenter();
+            $(".dimmed").fadeIn();
+            $(openTarget).fadeIn();
+        } else if ($this.attr("modal-control") == "close") {
+            $(".dimmed").fadeOut();
+            $(closeTarget).fadeOut();
+        } else if ($this.attr("modal-control") == "call") {
+            $(closeTarget).fadeOut();
+            modalCenter();
+            $(openTarget).fadeIn();
+        }
+        var _modal = e.target.closest(".modal");
+        if ($(_modal).find("iframe").length > 0) {
+            stopVideo(_modal);
+        }
+    });
+
+    //copyright
+    var dateGet = new Date();
+    $(".copyright .years").text(dateGet.getFullYear());
+
+    //home
+    $(".home").on("click", function(e){
+        e.preventDefault();
+        $("#blocks .mCSB_container").animate({
+            top: 0
+        }, 600, "easeOutCubic");
+        $("#blocks .mCSB_dragger").animate({
+            top: 0
+        }, 300, "easeOutCubic");
+    });
+    
+    //설정 탭
+    $('.tab li').on('click', function(){
+        index = $(this).index() + 1;
+        $('#set-type > div').each(function(){
+            $('.tab li').removeClass('on');
+        });
+        $(this).addClass('on');
+        $('#set-type > div').addClass('hidden');
+        $('.set' + index).removeClass('hidden');
+    });
+
+    //화면모드설정
+    var sel_txt = $('.select-box span, .select-box ul, .select-box i');
+    sel_txt.click(function(){
+        //console.log(sel_txt);
+        $('i').toggleClass('on');
+        $('.select-box ul').toggle();
+    });
+    sel_txt.find('button').click(function(e){
+        var $this = $(e.target);
+        $this.parents('li').addClass('on');
+        $this.parents('li').siblings().removeClass('on');
+        $('.select-box span').text($this.text());
+        /*if ($("#SetupApply").prop("disabled") == true) {
+            $("#SetupApply").prop("disabled", false)
+        }*/
+    });
+    $(".dimmed, #modal-set").click(function(e){
+        if($(e.target).closest(".select-box").length < 1) {
+            if ($('.select-box ul').is(':visible')) {
+                //alert('영역 밖입니다.'); 
+                $('i').removeClass('on');
+                $('.select-box ul').hide();
+            }
+        }
+    });
+
+    // resizing
+    resizing();
+});
+
+
+$(window).resize(function(){
+    modalCenter();
+    resizing();
+});
---
+++ js/login_design.js
@@ -0,0 +1,53 @@
+//launcher_login
+$(function(){
+    
+    //데이터센터 선택
+    $('.dc_select').click(function(){
+        $('.dc_txt').toggle();
+        $(this).find('i').toggleClass('on');
+    });
+
+    //데이터센터 선택 후 텍스트 값 가져오기
+    var select_c = $('.dc_txt li');
+    select_c.click(function(){
+        var select_c_name = $(this).find('span').text();
+        $('.dc_tit').text(select_c_name);
+    });
+
+///////////////////// 수정 190808
+    //채널 선택 시
+    var divLogobox = $('div.logoBox');
+    $('.logoBox').click(function(){
+        divLogobox.children('span').removeClass('ac na nx ka');
+        divLogobox.children('span').addClass('none').html('채널링 계정을 선택해주세요');
+        $('.ch_select i').addClass('on');
+        $('.ch_sBox, .dim').show();
+    });
+    
+    //채널선택 시 상단 채널로고 변경
+    var select_ch = $('.ch_sBox li');
+    select_ch.click(function(){
+        var select_ch_name = $(this).find('a').html();
+        $('.ch_select div').html(select_ch_name);
+        $('.ch_select i').removeClass('on');
+        $('.ch_sBox, .dim').hide();
+    });
+///////////////////// 수정 190808
+
+    //채널링 tab
+    $('.ch01').show();
+    $('.ch_sBox ul > li a').on('click', function(){
+        index = $(this).parent().index() + 1;
+        $('.ch_sBox ul > li a').each(function(){
+            $('.ch_sBox ul li').css('display:none');
+        });
+        $(this).parent().css('display:block');
+        $('.loginArea > div').addClass('hidden');
+        $('.ch0' + index).removeClass('hidden');
+    });
+
+    //1107 추가__ actoz pw focus off action
+    $(".actozArea .login_area").find('input[type="password"]').focusout(function(){
+        $(".pwtext").hide();
+    });
+});
